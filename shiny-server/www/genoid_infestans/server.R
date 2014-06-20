a3dd4e325efca5948f39e7930dbec9c622848d00
Javier F. Tabima <caifaz01@gmail.com>
2014-06-20 10:26:25 -0700

    fixed PDF output

--- shiny-server/www/genoid_infestans/server.R
+++ shiny-server/www/genoid_infestans/server.R
@@ -170,7 +170,7 @@ shinyServer(function(input, output) {
     filename = function() { paste0(input$tree, '.pdf') },
     content = function(file) {
       pdf(file, width=11, height=8.5)
-      plot.tree(tree, tip.col=as.character(unlist(data.genoid()$other$tipcolor)))
+      plot.tree(boottree(), tip.col=as.character(unlist(data.genoid()$other$tipcolor)))
       dev.off()
     })
   
